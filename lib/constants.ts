efb1700494083e07d3112d796d9f99da21efd212
Dylan Wilson <dwilson5817@gmail.com>
2023-01-15 17:48:32 +0000

    Use wildcard certificate for minersrealm.net
    Instead of requiring a new certificate to be issued each time a new
    project is created, wildcard certificates will be used instead.

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,4 +1,4 @@
 export class Constants {
     static readonly domainName: string = 'files.minersrealm.net'
-    static readonly certificateArn: string = 'arn:aws:acm:us-east-1:351642118781:certificate/c2a55bff-0e27-4efd-9739-60d42749bd80'
+    static readonly certificateArn: string = 'arn:aws:acm:us-east-1:351642118781:certificate/bd64141d-f868-4f8c-a2f1-8837dbd30e3e'
 }
