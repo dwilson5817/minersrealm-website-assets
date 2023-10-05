7af8955ed2eee8df6a2318acbd7ac4463076b8d0
Dylan Wilson <dwilson5817@gmail.com>
2023-10-05 22:25:50 +0000

    Change files.minersrealm.net to assets.minersrealm.net
    Migrate to new domain.  More descriptive for what is being hosted.

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,4 +1,4 @@
 export class Constants {
-    static readonly domainName: string = 'files.minersrealm.net'
+    static readonly domainName: string = 'assets.minersrealm.net'
     static readonly certificateArn: string = 'arn:aws:acm:us-east-1:965045848162:certificate/ce7d5b5b-aac7-49f2-8ca6-44b38a08276e'
 }
